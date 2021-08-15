7c2045cf0b7f0b7bc7fbfeed697246644adb6793
phantom05 <monster2jy@gmail.com>
2021-08-15 14:56:23 +0900

    api ser path

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiIndexRouter = require('./routes/api/index');
 
 var app = express();
 
@@ -21,7 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', indexRouter);
+app.use('/', indexRouter);
+app.use('/api', apiIndexRouter);
 app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -16,30 +16,8 @@ const dummyData = {
 }
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.json(dummyData.index)
-});
 
-router.get('/hello', function (req, res, next) {
-  res.json(dummyData.hello)
-});
-
-router.get('/todos', async function (req, res, next) {
-
-
-  const apiAddress = `https://jsonplaceholder.typicode.com/todos`;
-
-  const {data} = await axios({
-    method: "get",
-    url   : apiAddress
-  });
-  console.log(data)
-  data.length = 30
-
-  const responseData ={
-    data
-  }
-
-  res.json(responseData)
+  res.render('index')
 });
 
 
